e2f62f57725d399c6b8e2ee5f5e88db6d8f10d40
GartDev <gartdev@Garrett.localdomain>
2019-03-30 18:20:53 -0400

    Created generic table class with basic insert functionality

---
+++ Pouch/app/src/main/java/com/example/pouch/Table.java
@@ -0,0 +1,46 @@
+package com.example.pouch;
+
+public class Table {
+
+    int equips[][] = new int[10][10];
+
+    public int insert(int shapeID, int wepID) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (equips[i][j] == 0) {
+                    if (shapeID == 0) { //Square shapes
+                        if (equips[i + 1][j] == 0 && equips[i + 1][j + 1] == 0 && equips[i][j + 1] == 0) {
+                            equips[i][j] = wepID;
+                            equips[i + 1][j] = wepID;
+                            equips[i + 1][j + 1] = wepID;
+                            equips[i][j + 1] = wepID;
+                            return 1;
+                        }
+                    } else if (shapeID == 1) { //4-long shapes
+                        if (equips[i + 1][j] == 0 && equips[i + 2][j] == 0 && equips[i + 3][j] == 0) {
+                            equips[i][j] = wepID;
+                            equips[i + 1][j] = wepID;
+                            equips[i + 2][j] = wepID;
+                            equips[i + 3][j] = wepID;
+                            return 1;
+                        }
+                    } else if (shapeID == 1) { //3-long shapes
+                        if (equips[i + 1][j] == 0 && equips[i + 2][j] == 0) {
+                            equips[i][j] = wepID;
+                            equips[i + 1][j] = wepID;
+                            equips[i + 2][j] = wepID;
+                            return 1;
+                        }
+                    } else if (shapeID == 1) { //2-long shapes
+                        if (equips[i + 1][j] == 0) {
+                            equips[i][j] = wepID;
+                            equips[i + 1][j] = wepID;
+                            return 1;
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
