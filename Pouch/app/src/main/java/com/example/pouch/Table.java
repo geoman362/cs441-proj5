dc20c4437894a57cfbed7e9ca96e5dbf13cc4d2e
GartDev <gartdev@Garrett.localdomain>
2019-04-01 18:44:24 -0400

    Added mroe explicit insert function with x,y support

--- Pouch/app/src/main/java/com/example/pouch/Table.java
+++ Pouch/app/src/main/java/com/example/pouch/Table.java
@@ -43,4 +43,39 @@ public class Table {
         }
         return 0;
     }
+    public int insert(int shapeID, int wepID, int i, int j) {
+                if (equips[i][j] == 0) {
+                    if (shapeID == 0) { //Square shapes
+                        if (equips[i + 1][j] == 0 && equips[i + 1][j + 1] == 0 && equips[i][j + 1] == 0) {
+                            equips[i][j] = wepID;
+                            equips[i + 1][j] = wepID;
+                            equips[i + 1][j + 1] = wepID;
+                            equips[i][j + 1] = wepID;
+                            return 1;
+                        }
+                    } else if (shapeID == 1) { //4-long shapes
+                        if (equips[i + 1][j] == 0 && equips[i + 2][j] == 0 && equips[i + 3][j] == 0) {
+                            equips[i][j] = wepID;
+                            equips[i + 1][j] = wepID;
+                            equips[i + 2][j] = wepID;
+                            equips[i + 3][j] = wepID;
+                            return 1;
+                        }
+                    } else if (shapeID == 1) { //3-long shapes
+                        if (equips[i + 1][j] == 0 && equips[i + 2][j] == 0) {
+                            equips[i][j] = wepID;
+                            equips[i + 1][j] = wepID;
+                            equips[i + 2][j] = wepID;
+                            return 1;
+                        }
+                    } else if (shapeID == 1) { //2-long shapes
+                        if (equips[i + 1][j] == 0) {
+                            equips[i][j] = wepID;
+                            equips[i + 1][j] = wepID;
+                            return 1;
+                        }
+                    }
+                }
+        return 0;
+    }
 }
