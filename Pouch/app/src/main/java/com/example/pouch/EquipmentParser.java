4f523e0864622a2e000868328bd5057d998a9de7
Nate Hilscher <nhilsch1@binghamton.edu>
2019-04-05 17:56:04 -0400

    Restoring Order and Balance

--- Pouch/app/src/main/java/com/example/pouch/EquipmentParser.java
+++ Pouch/app/src/main/java/com/example/pouch/EquipmentParser.java
@@ -79,7 +79,7 @@ public class EquipmentParser {
             parseArmorTable(mediumTable, Armor.ArmorType.MEDIUM, content);
             content = armor.getJSONObject("Heavy Armor").getJSONArray("content");
             parseArmorTable(heavyTable, Armor.ArmorType.HEAVY, content);
-            
+
         }catch(Exception e){}
     }
 
