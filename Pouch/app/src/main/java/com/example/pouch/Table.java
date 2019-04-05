8253e4767424c332e5d7c24062705721fb730a4c
GartDev <gartdev@Garrett.localdomain>
2019-04-04 22:26:42 -0400

    Added delete functionality as well as constructor for different sized grids to table class

--- Pouch/app/src/main/java/com/example/pouch/Table.java
+++ Pouch/app/src/main/java/com/example/pouch/Table.java
@@ -2,11 +2,17 @@ package com.example.pouch;
 
 public class Table {
 
-    int equips[][] = new int[10][10];
-
+    private int equips[][];
+    private int xdim;
+    private int ydim;
+    public Table(int x, int y) {
+        equips = new int[x][y];
+        xdim = x;
+        ydim = y;
+    }
     public int insert(int shapeID, int wepID) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < xdim; i++) {
+            for (int j = 0; j < ydim; j++) {
                 if (equips[i][j] == 0) {
                     if (shapeID == 0) { //Square shapes
                         if (equips[i + 1][j] == 0 && equips[i + 1][j + 1] == 0 && equips[i][j + 1] == 0) {
@@ -77,5 +83,17 @@ public class Table {
                     }
                 }
         return 0;
+    }
+    public int remove(int wepID) {
+        int succ = 0;
+        for(int i = 0;i<xdim;i++){
+            for(int j = 0;j<ydim;j++){
+                if(equips[i][j] == wepID){
+                    equips[i][j] = 0;
+                    succ = 1;
+                }
+            }
+        }
+        return succ;
     }
 }
